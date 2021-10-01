b8f8952554acc2760ae7b4a01915a4ac486f7b3b
Austin Kammerer <austin4191@gmail.com>
2021-10-01 17:16:53 -0500

    added BS files, displayEmployee and calcMonthlyCost functions

--- client.js
+++ client.js
@@ -6,6 +6,7 @@ function readyNow() {
   // TODO: add click listener to submitBtn
   $("#submitBtn").on("click", submitEmployeeInfo);
   // TODO: add click listener to future deleteBtn (dynamic)
+  $("tbody").on("click", ".deleteBtn", deleteEmployee);
 }
 
 // TODO: function to collect input values and add to employee object array
@@ -16,7 +17,7 @@ function submitEmployeeInfo() {
   let lastName = $("#lastNameInput").val();
   let idNumber = $("#idInput").val();
   let jobTitle = $("#titleInput").val();
-  let annualSalary = $("#salaryInput").val();
+  let annualSalary = Number($("#salaryInput").val());
   let employee = {
     firstName: firstName,
     lastName: lastName,
@@ -34,10 +35,39 @@ function submitEmployeeInfo() {
   $("#salaryInput").val("");
   // pass the employee object into the display function
   displayEmployee(employee);
+  calcMonthlyCost();
 }
 
 // TODO: function to display employee info in table
-
+function displayEmployee(employee) {
+  // declare HTML to be added to DOM
+  let entry = $(`
+  <tr>
+    <td>${employee.firstName}</td>
+    <td>${employee.lastName}</td>
+    <td>${employee.idNumber}</td>
+    <td>${employee.jobTitle}</td>
+    <td>${employee.annualSalary}</td>
+    <td><button type="button" class="deleteBtn btn btn-danger">Delete</button></td>
+  </tr>
+  `);
+  // attach salary data to entry
+  entry.data("annualSalary", `${employee.annualSalary}`);
+  // append the employee entry
+  $("tbody").append(entry);
+}
 // TODO: function to calculate totalMonthlyCost - if greater than 20K, red background
-
+function calcMonthlyCost() {
+  // add up annualSalary of all employees
+  let totalMonthlyCost = employees.reduce(function (
+    previousValue,
+    currentValue
+  ) {
+    return previousValue + currentValue.annualSalary;
+  },
+  0);
+  // add to DOM
+  $("#totalCostOutput").text(totalMonthlyCost);
+}
 // TODO: function to delete employee entry from table
+function deleteEmployee() {}
