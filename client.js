cebce79729a78f1221a3b09217afd0c4976a0731
Austin Kammerer <austin4191@gmail.com>
2021-10-03 22:10:18 -0500

    added tableSort function

--- client.js
+++ client.js
@@ -11,6 +11,8 @@ function readyNow() {
   $("#submitBtn").on("click", submitEmployeeInfo);
   // TODO: add click listener to future deleteBtn (dynamic)
   $("tbody").on("click", ".deleteBtn", deleteEmployee);
+  // TODO: add click listeners to th elements for sortTable
+  $("#firstName, #lastName, #idNumber, #annualSalary").on("click", sortTable);
 }
 
 // TODO: function to collect input values and add to employee object array
@@ -104,4 +106,96 @@ function formatCurrency(number) {
     currency: "USD",
     maximumFractionDigits: 2,
   }).format(number);
+}
+let rows = [];
+// TODO: function to sort table by firstName, lastName, ID, annualSalary
+// researched at https://www.w3schools.com/howto/howto_js_sort_table.asp
+function sortTable() {
+  // grab the category for sorting based on the th clicked
+  let category = $(this).attr("id");
+  // select the table
+  let table = document.querySelector("table");
+  // select the rows of the table for sorting
+  rows = table.rows;
+  // declare sorting variables
+  let switching = true;
+  let direction = "ascend";
+  let shouldSwitch;
+  let i;
+  let switchcount = 0;
+  let x;
+  let y;
+  // loop while rows are being switched
+  while (switching) {
+    // flip switching so it can be flipped again if a row needs to move
+    switching = false;
+    // loop over table rows (not header row) and compare each row to the next one
+    for (i = 1; i < rows.length - 1; i++) {
+      shouldSwitch = false;
+      // loop over the td's and find the one we want to compare based on the th clicked
+      for (let td of rows[i].getElementsByTagName("td")) {
+        // use the data value we set in the submit function to find the correct td
+        if ($(td).data(category)) {
+          x = td;
+        }
+      }
+      // do the same for the row to compare to [i+1]
+      for (let td of rows[i + 1].getElementsByTagName("td")) {
+        if ($(td).data(category)) {
+          y = td;
+        }
+      }
+      // if in ascend mode, checks if current td is greater than the next row's td, if so, flag for switching and break the rows for loop
+      // the rows for loop (the first for loop) is broken when a row needs to switch in order to get to the code block that does the visual switching
+      if (direction === "ascend") {
+        // check if the data value is a string to normalize casing
+        if (typeof $(x).data(category) === "string") {
+          if (
+            $(x).data(category).toLowerCase() >
+            $(y).data(category).toLowerCase()
+          ) {
+            shouldSwitch = true;
+            break;
+          }
+        } else if (typeof $(x).data(category) === "number") {
+          if ($(x).data(category) > $(y).data(category)) {
+            shouldSwitch = true;
+            break;
+          }
+        }
+      }
+      // same concepts as the ascend block but comparisons are flipped
+      else if (direction === "descend") {
+        if (typeof $(x).data(category) === "string") {
+          if (
+            $(x).data(category).toLowerCase() <
+            $(y).data(category).toLowerCase()
+          ) {
+            shouldSwitch = true;
+            break;
+          }
+        } else if (typeof $(x).data(category) === "number") {
+          if ($(x).data(category) < $(y).data(category)) {
+            shouldSwitch = true;
+            break;
+          }
+        }
+      }
+    }
+    // checks if a row switch is needed
+    if (shouldSwitch) {
+      // if so, insert the second row before the first
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      // continue the while loop
+      switching = true;
+      switchcount++;
+    } else {
+      // if no switching in done in the current while iteration, the list is properly sorted
+      if (switchcount === 0 && direction === "ascend") {
+        // direction changes to reverse sorting when clicking on the same th again
+        direction = "descend";
+        switching = true;
+      }
+    }
+  }
 }
